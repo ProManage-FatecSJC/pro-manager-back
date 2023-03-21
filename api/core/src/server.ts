e36958810bff81aa336c143a471294f65dd42556
Ana Carolina dos Santos Rocha <78958795+annakks@users.noreply.github.com>
2023-03-21 20:22:47 -0300

    Fix: structure to deploy

---
+++ api/core/src/server.ts
@@ -0,0 +1,22 @@
+
+import express from 'express';
+import { AppDataSource } from './data-source';
+import routes from './routes'
+
+AppDataSource.initialize();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.json('ok')
+})
+
+app.use(routes)
+
+if (!module.parent) {
+    app.listen(process.env.PORT);
+    console.log('Express started on port 3000');
+}
+export default app;
