8cac331210743c0d560d548b50a1130502790474
Ana Carolina dos Santos Rocha <78958795+annakks@users.noreply.github.com>
2023-03-23 08:55:10 -0300

    Feat: Add cors

--- api/core/src/server.ts
+++ api/core/src/server.ts
@@ -2,12 +2,14 @@
 import express from 'express';
 import { AppDataSource } from './data-source';
 import routes from './routes'
+import cors from 'cors';
 
 AppDataSource.initialize();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     return res.json('ok')
